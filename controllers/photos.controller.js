bb0ceaac489aad776c47de359a445e8f94f15fcc
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-04-17 17:27:06 +0200

    Add file ext validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -11,9 +11,16 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
+      const acceptedExt = ['jpg', 'gif', 'png'];
+
+      if (acceptedExt.includes(fileExt)) {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong input!');
+      }
 
     } else {
       throw new Error('Wrong input!');
