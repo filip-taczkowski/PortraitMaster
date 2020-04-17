1f0aa7ba03150d69e7d4cb69c13fc9786623b07f
Filip Taczkowski <filip.taczkowski@gmail.com>
2020-04-17 17:16:55 +0200

    Init project

---
+++ controllers/photos.controller.js
@@ -0,0 +1,56 @@
+const Photo = require('../models/photo.model');
+
+/****** SUBMIT PHOTO ********/
+
+exports.add = async (req, res) => {
+
+  try {
+    const { title, author, email } = req.fields;
+    const file = req.files.file;
+
+    if(title && author && email && file) { // if fields are not empty...
+
+      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+      await newPhoto.save(); // ...save new photo in DB
+      res.json(newPhoto);
+
+    } else {
+      throw new Error('Wrong input!');
+    }
+
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
+
+/****** LOAD ALL PHOTOS ********/
+
+exports.loadAll = async (req, res) => {
+
+  try {
+    res.json(await Photo.find());
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
+
+/****** VOTE FOR PHOTO ********/
+
+exports.vote = async (req, res) => {
+
+  try {
+    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    else {
+      photoToUpdate.votes++;
+      photoToUpdate.save();
+      res.send({ message: 'OK' });
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+};
